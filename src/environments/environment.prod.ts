75778dc32fc84fb309de1658667cb869bd862e47
vetemi <valmir@codeschluss.de>
2024-04-29 08:24:21 +0200

    instance specific changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base: `${location.protocol}//${location.host}/`,
-  client: `${location.protocol}//${location.host}/`,
+  base: 'https://demo.joinmilo.de/',
+  client: 'https://demo.joinmilo.de/',
 };
